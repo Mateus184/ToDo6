f90886174fd98f69c8d5762c1a22c0f491f76edc
Mateus184 <mateussilva184@hotmail.com>
2022-09-26 23:34:22 -0300

    Finalização do ToDo6

--- ToDo6/Create Table Avaliacao.sql
+++ ToDo6/Create Table Avaliacao.sql
@@ -1,14 +1,15 @@
+create database todo6;
+
 use ToDo6;
 
 create table Avaliacao
 
 (
-id_avaliacao smallint,
+id_avaliacao int,
 titulo varchar (64),
 produto varchar (20) not null,
-data_avaliação date,
-nota smallint (1)
+data_avaliação varchar(10),
+nota smallint (1),
+cliente varchar(64)
 
 );
-
-alter table Avaliacao add column (Cliente date);
---
+++ ToDo6/insert into Avaliacao.sql
@@ -0,0 +1,327 @@
+use Todo6;
+
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 7861, 'Médio', 'Som', 15-11-1991, 5, 'Cachorro do Bill');
+
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 57862, 'Ruim', 'notebook', 10-01-1990, 3, 'Bill');
+
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 769, 'Bom', 'moto_g_90', 10-11-1991, 3, 'Xuxa');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 789767575, 'Médio', 'Som', 15-11-2020, 5, 'Bill');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 988456, 'Bom', 'moto_g_90', 10-11-2021, 3, 'asas');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 7888575, 'Médio', 'Som', 15-11-1991, 5, 'Billu');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 98, 'Bom', 'moto_g_90', 10-11-2021, 3, 'Xuxau');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 779575, 'Médio', 'Som', 15-11-1991, 5, 'Joana');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 779, 'Bom', 'moto_g_90', 10-11-2021, 3, 'Xuxa');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 7556775, 'Médio', 'Som', 15-11-2020, 5, 'Bill');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 9, 'Bom', 'moto_g_90', 10-11-2021, 3, 'Xuxa');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 7545645675, 'Médio', 'Som', 15-11-2020, 5, 'Bill');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 229, 'Bom', 'moto_g_90', 10-11-2021, 3, 'Xuxa');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 72575, 'Médio', 'Som', 15-11-2020, 5, 'Bill');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value (97899, 'Bom', 'moto_g_90', 10-11-2021, 3, 'Xuxa');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 7338, 'Médio', 'Som', 15-11-2020, 5, 'Bill');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 1323, 'Médio', 'Som', 15-11-2020, 5, 'Bill');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 19, 'Bom', 'moto_g_90', 10-11-2021, 3, 'Xuxa');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 3838, 'Médio', 'Som', 15-11-2020, 5, 'Bill');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 744575, 'Médio', 'Som', 15-11-2020, 5, 'Bill');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value (453839, 'Bom', 'moto_g_90', 10-11-2021, 3, 'Xuxa');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 7575, 'Médio', 'Som', 15-11-2020, 5, 'Bill');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 45383, 'Médio', 'Som', 15-11-2020, 5, 'Bill');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 9, 'Bom', 'moto_g_90', 10-11-2021, 3, 'Xuxa');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 345683, 'Médio', 'Som', 15-11-2020, 5, 'Bill');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 4568, 'Médio', 'Som', 15-11-2020, 5, 'Bill');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 9, 'Bom', 'moto_g_90', 10-11-2021, 3, 'xa');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 75456875, 'Médio', 'Som', 15-11-2020, 5, 'Bill');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 4567867575, 'Médio', 'Som', 15-11-2020, 5, 'Bill');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 976459, 'Bom', 'moto_g_90', 10-11-2021, 3, 'Xuxa');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 347575, 'Médio', 'Som', 15-11-2020, 5, 'Bill');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 755478975, 'Médio', 'Som', 15-11-2020, 5, 'Bill');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 9786, 'Bom', 'moto_g_90', 10-11-2021, 3, 'Xuxa');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 4654565, 'Médio', 'Som', 15-11-2020, 5, 'Bill');insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 988456, 'Bom', 'moto_g_90', 10-11-2021, 3, 'asas');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 7888575, 'Médio', 'Som', 15-11-1991, 5, 'Billu');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 98, 'Bom', 'moto_g_90', 10-11-2021, 3, 'Xuxau');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 779575, 'Médio', 'Som', 15-11-1991, 5, 'Bill');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 779, 'Bom', 'moto_g_90', 10-11-2021, 3, 'Xuxa');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 7556775, 'Médio', 'Som', 15-11-2020, 5, 'Bill');insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 988456, 'Bom', 'moto_g_90', 10-11-2021, 3, 'asas');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 7888575, 'Médio', 'Som', 15-11-1991, 5, 'Billu');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 98, 'Bom', 'moto_g_90', 10-11-2021, 3, 'Xuxau');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 779575, 'Médio', 'Som', 15-11-1991, 5, 'Bill');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 779, 'Bom', 'moto_g_90', 10-11-2021, 3, 'Xuxa');
+insert into avaliacao (
+id_avaliacao,
+titulo,
+produto ,
+data_avaliação ,
+nota,
+cliente) value ( 7556775, 'Médio', 'Som', 15-11-2020, 5, 'Bill');
+
+
+
